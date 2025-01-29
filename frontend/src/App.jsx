7bbd1c7350af69e32a585e773e9a140e2aa9f724
Mridul <140313918+Mridul-23@users.noreply.github.com>
2025-01-29 06:55:01 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl uppercase font-bold text-center m-10 text-black font-sans'>Project Started</h1>
+    </>
+  )
+}
+
+export default App
